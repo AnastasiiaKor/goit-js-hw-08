f92c095a395b72854a25d0dd45aa442f69a1b782
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2022-12-09 01:47:43 +0500

    hopefully last update to task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,21 @@ const iframeEl = document.querySelector('#vimeo-player');
 const vimeoPlayer = new Player(iframeEl);
 
 vimeoPlayer.on('timeupdate', throttle(onPlay, 1000));
+setTime();
 
 function onPlay({ seconds, duration }) {
   localStorage.setItem('videoplayer-current-time', seconds);
   if (seconds === duration) {
-    localStorage.setItem('videoplayer-current-time', 0);
+    localStorage.removeItem('videoplayer-current-time');
   }
 }
-vimeoPlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function setTime() {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    vimeoPlayer.setCurrentTime(
+      localStorage.getItem('videoplayer-current-time')
+    );
+  } else {
+    vimeoPlayer.setCurrentTime(0);
+  }
+}
